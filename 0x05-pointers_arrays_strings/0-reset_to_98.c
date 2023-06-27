d8298d61d3401d103fec73a84b54b25af9afd30e
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-06-27 10:34:33 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets a variable to 98 via its pointer
+ * @n: pointer to an int to be reset
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README inside my folder 0x05-pointers_arrays_strings
