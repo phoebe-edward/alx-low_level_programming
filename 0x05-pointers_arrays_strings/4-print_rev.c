56a1b9352719278ff35b327ea7e590761dea9b50
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-06-27 11:59:35 -0700

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ *@s: pointer to a character array
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
