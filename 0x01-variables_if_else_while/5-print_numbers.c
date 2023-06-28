097abfdfd2dbb874e9274c029ae223a36f7835e0
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-06-28 10:38:43 -0700

    print all signle decimal digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
