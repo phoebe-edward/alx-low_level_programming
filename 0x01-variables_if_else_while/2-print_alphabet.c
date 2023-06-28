deb49835ba963408da6d1fd8d2fe51c54322b405
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-06-28 08:36:34 -0700

    print all alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
