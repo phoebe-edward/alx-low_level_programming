1639731b51be32fd8a23abf952a11867400913dd
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-07-01 09:53:59 -0700

    modified times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,11 @@ void times_table(void)
 				_putchar(' ');
 			}
 			res = i * j;
-			if (res < 10)
+			if (j == 0)
+			{
+				_putchar(res + '0');
+			}
+			else if (res < 10)
 			{
 				_putchar(' ');
 				_putchar(res + '0');
