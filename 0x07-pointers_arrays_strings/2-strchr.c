e9e490fe39d8d1c4ac57517bec31e58a79a5cc5e
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-07-03 13:53:15 -0700

    search for character c in s

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - searches for character c in string s
+ * @s: pointer t character array (string)
+ * @c: character to be searched within s
+ * Return: pointer to first occurrence of c in s, or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (&s[i]);
+}
