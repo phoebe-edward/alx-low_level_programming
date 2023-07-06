522487ee341e5888dc7d5bdacfc17f606c681b2b
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-07-06 06:27:18 -0700

    wildcompare edited

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -25,7 +25,7 @@ int wildcmp(char *s1, char *s2)
 		if (*(s2 + 1) == '*' || *s1 == *(s2 + 1))
 			return (wildcmp(s1, s2 + 1));
 		if (*(s1 + 1) == *(s2 + 1))
-			return (wildcmp(s1 + 1, s2 + 2));
+			return (wildcmp(s1 + 1, s2 + 1));
 		return (wildcmp(s1 + 1, s2));
 	}
 	if (*s1 != *s2 && *s2 != '*' && *(s2 - 1) == '*')
