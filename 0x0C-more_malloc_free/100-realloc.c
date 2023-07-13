fe7fa6e3ea335e5abebfcd3b5fe59fd7d8e091c9
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-07-13 08:44:38 -0700

    edited 2 realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,6 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr_new = malloc(new_size);
 		if (ptr_new == NULL)
 			return (NULL);
+		return(ptr_new);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
