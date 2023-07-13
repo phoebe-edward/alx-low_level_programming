6a9534987666fe5cf6ace6b27d386975d4c6707d
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-07-13 08:21:53 -0700

    make realloc yourself

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to old memory
+ * @old_size: old size
+ * @new_size: new size
+ * Return: void * to new memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr_new, *copy;
+	unsigned int min, i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		ptr_new = malloc(new_size);
+		if (ptr_new == NULL)
+			return (NULL);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr_new = malloc(new_size);
+	if (ptr_new == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (old_size < new_size)
+		min = old_size;
+	else
+		min = new_size;
+	copy = ptr;
+	for (i = 0; i < min; i++)
+	{
+		ptr_new[i] = copy[i];
+	}
+	free(ptr);
+	return (ptr_new);
+}
