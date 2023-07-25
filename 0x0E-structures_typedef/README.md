bb4b2566eed5b4b95c351a1c2e7e4eafb42452c6
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-07-25 04:33:21 -0700

    structure of the dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+README inside folder 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_structure
+#define dog_structure
+/**
+ * struct dog - structure of dog
+ * @name: its name
+ * @age: its age
+ * @owner: name of its owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
