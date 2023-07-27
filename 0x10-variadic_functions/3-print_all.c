b867c4421b71757ecd544668210cf3bb6c495bd1
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-07-27 07:32:59 -0700

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,72 @@
+#include "variadic_functions.h"
+/**
+ * _print_char - prints char
+ * @list: list
+ */
+void _print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+/**
+ * _print_int - print int
+ * @list: list
+ */
+void _print_int(va_list list)
+{
+	printf("%i", va_arg(list, int));
+}
+/**
+ * _print_float - print float
+ * @list: list
+ */
+void _print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+/**
+ * _print_string - print string
+ * @list: list
+ */
+void _print_string(va_list list)
+{
+	char *s = va_arg(list, char*);
+	if (s != NULL)
+	{
+		printf("%s", s);
+		return;
+	}
+	printf("(nil)");
+}
+/**
+ * print_all - prints all
+ * @format: format of variables to be printed
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, j;
+	form_t arr[] = {
+		{'c', _print_char},
+		{'i', _print_int},
+		{'f', _print_float},
+		{'s', _print_string}};
+
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == arr[j].c)
+			{
+				if (i != 0)
+					printf(", ");
+				arr[j].func(args);
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,19 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ * struct form - structure
+ * @c: char element
+ * @func: function pointer element
+ */
+typedef struct form
+{
+	char c;
+	void (*func)(va_list);
+} form_t;
+void print_all(const char * const format, ...);
+void _print_char(va_list list);
+void _print_int(va_list list);
+void _print_float(va_list list);
+void _print_string(va_list list);
 #endif
