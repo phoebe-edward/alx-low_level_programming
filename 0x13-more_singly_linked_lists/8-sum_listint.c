2c9242f8bcb3faae626a9651758e8003f25536a1
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-08-01 12:38:23 -0700

    sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_listint - sum of list integers
+ * @head: pointer to the head of list
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *ptr;
+
+	if (head == NULL)
+		return (0);
+	ptr = head;
+	while (ptr != NULL)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+	return (sum);
+}
