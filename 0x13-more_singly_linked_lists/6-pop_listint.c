faea1d1234fc902bfcfebc038c1e9bbd20a12a2a
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-08-01 08:16:46 -0700

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - gets the first element out
+ * @head: pointer to pointer to the list
+ * Return: the int poped out
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *ptr;
+
+	if (head == NULL)
+		return (0);
+	n = (*head)->n;
+	ptr = (*head)->next;
+	free(*head);
+	*head = ptr;
+	return (n);
+}
