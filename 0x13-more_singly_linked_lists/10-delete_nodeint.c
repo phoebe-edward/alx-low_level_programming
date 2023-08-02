fbd8bba51d317be66acc193e91189fd5896b956e
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-08-02 05:47:50 -0700

    edited betty delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 1;
 	listint_t *ptr1, *ptr2;
-	
+
 	if (head == NULL || *head == NULL)
 		return (-1);
 	ptr1 = *head;
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		ptr1 = ptr1->next;
 		ptr2 = ptr1->next;
-                i++;
+		i++;
 	}
 	return (-1);
 }
