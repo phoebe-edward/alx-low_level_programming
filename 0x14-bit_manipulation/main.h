16f3db9a001499a5a4467322d109f68389de2051
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-08-03 08:38:39 -0700

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,4 +2,6 @@
 #define MAIN_H
 #include <stdlib.h>
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+void print_binary(unsigned long int n);
 #endif
---
+++ 0x14-bit_manipulation/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("integer: %d\n", 5<<1);
+	printf("character: %c\n", 5<<1);
+	return (0);
+}
