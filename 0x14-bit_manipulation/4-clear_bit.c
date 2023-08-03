7c85a0dc5cc8cf54afe6e8ae02650cf179c931b7
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-08-03 14:18:36 -0700

    set bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * clear_bit - set the bit at index to 0
+ * @n: pointyer to num
+ * @index: index
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int num, trial;
+
+	if (index > 64)
+		return (-1);
+	trial = *n >> index;
+	if (trial & 1)
+	{
+		num = 1 << index;
+		*n = *n ^ num;
+		return (1);
+	}
+	else
+	{
+		return (1);
+	}
+}
