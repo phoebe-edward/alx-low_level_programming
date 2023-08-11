d6957ee95487a805b030d735baa2a465563f7904
Phoebe Edward <phoebe.edward.kirelloss@gmail.com>
2023-08-11 16:43:03 -0700

    edited copy file 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,29 +20,28 @@ int main(int ac, char **av)
 	if (access(av[2], F_OK) == -1)
 		fd_to = creat(av[2], 0664);
 	else
-		fd_to = open(av[2], O_WRONLY | O_TRUNC);
+		fd_to = open(av[2], O_CREAT, O_WRONLY | O_TRUNC);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	buf = malloc(1024 * sizeof(char));
 	while (size_r > 0)
 	{
 		size_r = read(fd_from, buf, 1024);
 		if (access(av[1], F_OK) == -1 || size_r == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-		size_w = write(fd_to, buf, size_r);
-		if (fd_to == -1 || size_w == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		if (size_r > 0)
+		{
+			size_w = write(fd_to, buf, size_r);
+			if (size_w == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		}
 	}
 	i = close(fd_from);
 	if (i == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd_from);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd_from), exit(100);
 	j = close(fd_to);
 	if (j == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd_to);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd_to), exit(100);
 	free(buf);
 	return (0);
 }
