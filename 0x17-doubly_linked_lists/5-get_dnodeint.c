1899db22cb2240018b3494fc30148a35ee142120
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-09-14 10:18:40 -0700

    get required node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to list
+ * @index: index of the required node
+ * Return: pointer to required node or NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *node;
+	unsigned int count;
+
+	if (head == NULL)
+		return (NULL);
+	if (index == 0)
+		return (head);
+	node = head;
+	count = 0;
+	while (count != index && node != NULL)
+	{
+		node = node->next;
+		count++;
+	}
+	return (node);
+}
