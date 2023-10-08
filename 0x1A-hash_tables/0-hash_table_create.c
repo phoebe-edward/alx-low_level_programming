9161c10629fadfce5bb3e8038fb98e330677b896
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-08 07:29:01 -0700

    edited hash table create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = NULL;
+	hash_table_t *hash_table;
 	unsigned long int i;
 
 	if (size == 0)
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
-	hash_table->array = malloc(size * sizeof(hash_node_t));
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
